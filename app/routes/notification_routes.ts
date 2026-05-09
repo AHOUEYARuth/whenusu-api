cbc0213c95ed18d349b05a49e35356688195f79c
Ruth AHOUEYA <ruahoueya@gmail.com>
2026-05-09 21:24:13 +0100

    update notifications routes

--- app/routes/notification_routes.ts
+++ app/routes/notification_routes.ts
@@ -9,8 +9,8 @@ export const NotificationRoutes = () => {
     .group(() => {
       router.get('/', [NotificationsController, 'index'])
       router.get('/:id', [NotificationsController, 'show']).where('id', router.matchers.uuid())
-      router.post('/', [NotificationsController, 'store'])
-      router.put('/:id', [NotificationsController, 'update']).where('id', router.matchers.uuid())
+      // router.post('/', [NotificationsController, 'store'])
+      // router.put('/:id', [NotificationsController, 'update']).where('id', router.matchers.uuid())
       router.delete('/:id', [NotificationsController, 'destroy']).where('id', router.matchers.uuid())
       
       router.patch('/:id/read', [NotificationsController, 'markAsRead']).where('id', router.matchers.uuid())
